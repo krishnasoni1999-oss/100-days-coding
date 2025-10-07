108bf79c8b2f4d701002c56b223b565105156b40
krishnasoni1999-oss <krishnasoni9232@gmail.com>
2025-10-07 22:38:58 +0530

    Add files via upload

---
+++ countpn.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    int pos = 0, neg = 0, zero = 0;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if (arr[i] > 0) {
+            pos++;
+        } else if (arr[i] < 0) {
+            neg++;
+        } else {
+            zero++;
+        }
+    }
+    printf("Positive=%d, Negative=%d, Zero=%d\n", pos, neg, zero);
+    getchar();
+    getchar();
+    return 0;
+}
